13d578cfd0448b33c9f8f6e0eef96e3984534de5
taylerbruin <taylerbruin@gmail.com>
2022-05-26 09:11:58 -0600

    changed home to icon added some grid stuff

--- basescript.js
+++ basescript.js
@@ -1,37 +1,37 @@
-function createheader(){
+function createheader() {
 
-return `
-<meta charset="uft-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link rel="stylesheet" href="style.css">
-<title>Tayler Bruin</title>
-<div id = "heading" class = "heading">
-<button class="material-icons" onclick="location.href='index.html'" type="button">
-home</button>
-<nav>
-    <div class="dropdown">
-        <button class="dropdownbutton">Creative Projects</button>
-        <div class ="dropdown-content">
-            <a href="comms_taki.html">Taki</a>
-            <a href="byupromo.html">byupromo</a>
-        </div>
+    return `
+    <meta charset="uft-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
+    <title>Tayler Bruin</title>
+    <div id="heading" class="heading">
+        <button class="home-button material-icons" onclick="location.href='index.html'" type="button">
+            home</button>
+        <nav>
+            <div class="dropdown">
+                <button class="dropdownbutton">Creative Projects</button>
+                <div class="dropdown-content">
+                    <a href="comms_taki.html">Taki</a>
+                    <a href="byupromo.html">byupromo</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropdownbutton">Technical Projects</button>
+                <div class="dropdown-content">
+                    <a href="githubprojects.html">githubprojects</a>
+                </div>
+            </div>
+        </nav>
     </div>
-    <div class="dropdown">
-    <button class="dropdownbutton">Technical Projects</button>
-        <div class ="dropdown-content">
-            <a href="githubprojects.html">githubprojects</a>
-        </div>
-    </div>
-</nav>
-</div>
 `
 }
 this.mainheader();
 
-function mainheader(){
+function mainheader() {
     console.log(createheader());
     //creates base header for all pages.
     document.getElementById("top").innerHTML += createheader();
 
-    
+
 }
