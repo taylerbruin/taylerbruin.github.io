74580da91f4fd10b6ef77ee4e33b5bceadf1d4a1
taylerbruin <taylerbruin@gmail.com>
2022-06-01 09:42:39 -0600

    toggle drop down 75% working needs outside click.

--- basescript.js
+++ basescript.js
@@ -1,37 +1,49 @@
 function createheader() {
-
+    
     return `
     <meta charset="uft-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css">
-    <title>Tayler Bruin</title>
-    <div id="heading" class="heading">
-        <button class="home-button material-icons" onclick="location.href='index.html'" type="button">
-            home</button>
-        <nav class = "navigation_bar">
-            <div class="dropdown">          
-                <button class="dropdownbutton">Creative Projects <i class="arrow down"></i> </button>
-                <div class="dropdown-content">
-                    <a href="comms_taki.html">Taki</a>
-                    <a href="byupromo.html">byupromo</a>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="style.css">
+        <title>Tayler Bruin</title>
+        <div id="heading" class="heading">
+            <button class="home-button material-icons" onclick="location.href='index.html'" type="button">
+                home</button>
+            <nav class = "navigation_bar">
+                <div class="dropdown">          
+                    <button class="dropdownbutton" onclick="toggle_dropdown('dropdownmenu_creative' , 'displayarrow_creative')">Creative Projects <i id="displayarrow_creative"class="arrow down"></i> </button>
+                    <div id="dropdownmenu_creative" class="dropdown-content">
+                        <a href="comms_taki.html">Taki</a>
+                        <a href="byupromo.html">byupromo</a>
+                    </div>
                 </div>
-            </div>
-            <div class="dropdown">
-                <button class="dropdownbutton">Technical Projects <i class="arrow down"></i> </button>
-                <div class="dropdown-content">
-                    <a href="githubprojects.html">githubprojects</a>
+                <div class="dropdown">
+                    <button class="dropdownbutton" onclick="toggle_dropdown('dropdownmenu_technical' , 'displayarrow_technical')">Technical Projects <i id="displayarrow_technical"class="arrow down"></i> </button>
+                    <div id="dropdownmenu_technical" class="dropdown-content">
+                        <a href="githubprojects.html">githubprojects</a>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </div>
+            </nav>
+        </div>
 `
 }
 this.mainheader();
 
 function mainheader() {
-    console.log(createheader());
     //creates base header for all pages.
     document.getElementById("top").innerHTML += createheader();
 
 
+}
+
+function toggle_dropdown(menu_id , arrow_id){
+    console.log(menu_id);
+    if(document.getElementById(menu_id).classList.contains("dropdown-content")){
+        document.getElementById(menu_id).className = "dropdown-content-visible";
+        document.getElementById(arrow_id).className = "arrow up";
+
+    }
+    else if (document.getElementById(menu_id).classList.contains("dropdown-content-visible")){
+        document.getElementById(menu_id).className = "dropdown-content";
+        document.getElementById(arrow_id).className = "arrow down";
+    }
 }
